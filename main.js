eedcf748abf9297799076e5291a2e8a498a18e43
Dary-Boyce <contact@darylboyce.com>
2021-11-25 11:16:37 +0000

    Mobile version complete

--- main.js
+++ main.js
@@ -1,67 +1,72 @@
 //This is the text typewriter code
 var typed = new Typed('.hero__typer', {
-    strings: ["My name is Daryl Boyce"],
-    // these are the sentences that get typed out
-    showCursor: false,
-    // this disabled the standard cursor
-    typeSpeed: 100,
-    smartBackspace: true,
-    backSpeed: 100,
-    backDelay: 1000,
-    // These set the speed and the amount of characters removed
-    
-    // These rules set the looping aspect so that the typewriter never stops
-  });
+  strings: ["My name is Daryl Boyce"],
+  // these are the sentences that get typed out
+  showCursor: false,
+  // this disabled the standard cursor
+  typeSpeed: 100,
+  smartBackspace: true,
+  backSpeed: 100,
+  backDelay: 1000,
+  // These set the speed and the amount of characters removed
+
+  // These rules set the looping aspect so that the typewriter never stops
+});
 ///
 ///
 ///This is the pop down menu code.
-$(function (){
+$(function () {
 
   // hide main menu
   $('.nav__drop__down').hide();
-  $('.menu-btnx').hide();  
+  $('.menu-btnx').hide();
 
   // select menu-button and toggles between the X and burger symbol.
-  $('.menu-btn, .menu-btnx').click(function (){ 
+  $('.menu-btn, .menu-btnx').click(function () {
     $('.nav__drop__down').toggle(370); // expands menu
     $('.menu-btn').toggle(200);
     $('.menu-btnx').toggle(400);
   });
 
   // hides sub-menu
-  $('.sub-menu').hide(); 
+  $('.sub-menu').hide();
 
   // selects button to expand menu
-  $('.nav li.expand').click(function(){ 
+  $('.nav li.expand').click(function () {
     $('.sub-menu').toggle(300); // shows sub-menu
   });
-  
+
 });
 ///
 ///
 ///This is for the form validation
 lower__form.addEventListener('submit', (e) => {
   const email__check = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  
-  e.preventDefault();
-  
-  if (first__name.value == "") {
-      alert("Enter a your  first name");
-  }else
-  if (last__name.value == ""){
-    alert("Enter a your last name");
-    }
-  
-    if ( email__address.value == "" ) {
-    alert("Enter a Valid Email");
-    }else
 
-    if ( email__address.value.match(email__check) ) {
-    
-    } else
+  e.preventDefault();
+
+  if (first__name.value == "") {
+    alert("Enter a your  first name");
+  } else
+
+  if (last__name.value == "") {
+    alert("Enter a your last name");
+  }else
+
+  if (email__address.value == "") {
+    alert("Enter a Valid Email");
+  } else
+
+  if (email__address.value.match(email__check)) {
+  } else
     alert("Enter a Vaild Email");
   
+  if (subject.value == "") {
+    alert("Enter a Vaild Subject");
+  } else
+
+  if (email__address.value == "") {
+    alert("Enter a Valid Message");
+  } 
+
 })
-
-
-// [^acb].text(first__name.value)
