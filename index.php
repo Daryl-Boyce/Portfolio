3051fafdddb89632b21c2127383c76fe5345b644
Dary-Boyce <contact@darylboyce.com>
2022-04-11 17:58:03 +0100

    Fixed HTML conflict and css mismatch.

--- index.php
+++ index.php
@@ -7,33 +7,7 @@
     include ('header.php');
     include ('contact__submit.php');
 ?>
-<body class="home__page__body">
-    
-            <!-- <a href="#" class="main__inner__nav__contact">
-            contact me
-            </a> -->
-            <!-- Social media icon links -->
-           <!-- <div class="main__inner__nav__social"> -->
-                
-                <!--  -->
-                <!-- <div class="main__inner__nav__social__twitter">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                </div> -->
-                <!--  -->
-                <!-- <div class="main__inner__nav__social__instagram">
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                </div> -->
-                <!--  -->
-                <!-- <div class="main__inner__nav__social__facebook">
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                </div> -->
-                <!--  -->
-                <!-- <div class="main__inner__nav__social__linkedin">
-                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                </div> -->
-                <!--  -->
-                <!--  -->
-            <!-- </div> -->
+<body>
         </div>
     </nav>
     <div class="home__page__body__inner">
@@ -43,20 +17,14 @@
             <!--  -->
             <!-- Hero banner main page -->
             <div class="hero__banner">
-                <!-- <div class= "hero__banner__typing">
-                    <h1>My Name is Daryl Boyce</h1>
-                </div>
-                <div class="hero__banner__fade__text">
-                    <h2>I'm a Web Developer</h2>
-                </div>     -->
                 <div class="container"></div>
-
-
             </div>
             <!--  -->
             <!--  -->
         </header>
-        <!--  -->
+        
+    </div>
+    <!--  -->
         <!-- Main section of the home page -->
         <div class="inner__content">
             <!--  -->
@@ -70,12 +38,12 @@
                     <div class="projects__inner__card">
                         <!--  -->
                         <!-- image for the cards -->
-                        <a href="#" id="projects__inner__cards__image" class="projects__inner__cards__image">
+                        <a href="https://wordpress.daryl-boyce.netmatters-scs.co.uk/" id="woocommerce__image" >
                         </a>
                         <div class="projects__inner__card__lower">
-                            <h2>project one</h2>
+                            <h2>Wordpress WooCommerce Website</h2>
                             <div class="projects__inner__card__lower__aside">
-                                <h4>veiw project</h4>
+                                <a href="https://wordpress.daryl-boyce.netmatters-scs.co.uk/">veiw project</a>
                                 <i class="fas fa-arrow-right"></i>
                             </div>
                         </div>
@@ -87,84 +55,16 @@
                     <div class="projects__inner__card">
                         <!--  -->
                         <!-- image for the cards -->
-                        <a href="#" class="projects__inner__cards__image">
+                        <a href="#" id="netmatters__reflection">
                         </a>
                         <div class="projects__inner__card__lower">
-                            <h2>project two</h2>
+                            <h2>netmatters webpage</h2>
                             <div class="projects__inner__card__lower__aside">
-                                <h4>veiw project</h4>
+                                <h4>view project</h4>
                                 <i class="fas fa-arrow-right"></i>
                             </div>
                         </div>
-                        <!--  -->
-                        <!--  -->
-                    </div>
-                    <!--  -->
-                    <!-- HTML for cards  -->
-                    <div class="projects__inner__card">
-                        <!--  -->
-                        <!-- image for the cards -->
-                        <a href="#" class="projects__inner__cards__image">
-                        </a>
-                        <div class="projects__inner__card__lower">
-                            <h2>project three</h2>
-                            <div class="projects__inner__card__lower__aside">
-                                <h4>veiw project</h4>
-                                <i class="fas fa-arrow-right"></i>
-                            </div>
-                        </div>
-                        <!--  -->
-                        <!--  -->
-                    </div>
-                     <!-- HTML for cards  -->
-                     <div class="projects__inner__card">
-                        <!--  -->
-                        <!-- image for the cards -->
-                        <a href="#" class="projects__inner__cards__image">
-                        </a>
-                        <div class="projects__inner__card__lower">
-                            <h2>project four</h2>
-                            <div class="projects__inner__card__lower__aside">
-                                <h4>veiw project</h4>
-                                <i class="fas fa-arrow-right"></i>
-                            </div>
-                        </div>
-                        <!--  -->
-                        <!--  -->
-                    </div>
-                    <!--  -->
-                    <!-- HTML for cards  -->
-                    <div class="projects__inner__card">
-                        <!--  -->
-                        <!-- image for the cards -->
-                        <a href="#" class="projects__inner__cards__image">
-                        </a>
-                        <div class="projects__inner__card__lower">
-                            <h2>project five</h2>
-                            <div class="projects__inner__card__lower__aside">
-                                <h4>veiw project</h4>
-                                <i class="fas fa-arrow-right"></i>
-                            </div>
-                        </div>
-                        <!--  -->
-                        <!--  -->
-                    </div>
-                    <!--  -->
-                    <!-- HTML for cards  -->
-                    <div class="projects__inner__card">
-                        <!--  -->
-                        <!-- image for the cards -->
-                        <a href="#" class="projects__inner__cards__image">
-                        </a>
-                        <div class="projects__inner__card__lower">
-                            <h2>project six</h2>
-                            <div class="projects__inner__card__lower__aside">
-                                <h4>veiw project</h4>
-                                <i class="fas fa-arrow-right"></i>
-                            </div>
-                        </div>
-                        <!--  -->
-                        <!--  -->
+                        
                     </div>
                     <!--  -->
                 </div>
@@ -178,21 +78,7 @@
                 <hr>
                 <div class="lower__form__inner__description">
                     <h1>get in touch</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra erat tellus, 
-                        pulvinar gravida massa tincidunt id. Morbi pulvinar mauris felis, sed vestibulum urna
-                        venenatis eu.
-                    </p>
-                    
-                    <h3>000000</h3>
-
                     <a href="#">contact@darylboyce.com</a>
-                    
-                    <p>
-                        Nulla ut nunc lectus. Vivamus volutpat diam ac bibendum iaculis. Nulla facilisi.
-                        Suspendisse iaculis, justo in auctor viverra, leo erat aliquet enim, et varius arcu 
-                        leo nec purus. Mauris tincidunt condimentum nisl, vel vulputate elit dictum vel.
-                    </p>
                 </div>
                 <div class="lower__form__inner__inputs">
                     <form>
@@ -206,8 +92,6 @@
                 </div>
             </div>
         </div>
-    </div>
-
    
     
 
